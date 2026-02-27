2a302531331d66c9360b58dec0f908bd7386e457
fabinn12 <130874870+fabinn12@users.noreply.github.com>
2026-02-26 21:16:54 -0300

    Update App.jsx

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import { ScrollToTop } from './components/ScrollToTop';
+import  ScrollToTop  from './components/ScrollToTop';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import HomePage from './pages/HomePage.jsx';
