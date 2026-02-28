2aac9fe2af210a2f9f7991f99ae21006cb2ee389
fabinn12 <130874870+fabinn12@users.noreply.github.com>
2026-02-27 21:05:42 -0300

    Create AuthContext.jsx

---
+++ apps/web/src/contexts/AuthContext.jsx
@@ -0,0 +1,11 @@
+import { supabase } from "@/lib/supabaseClient";
+
+async function loginWithPassword(email, password) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) throw error;
+  return data;
+}
