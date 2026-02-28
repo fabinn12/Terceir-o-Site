56bd5bffda564003d19279c39ce00574c0275f66
fabinn12 <130874870+fabinn12@users.noreply.github.com>
2026-02-27 21:18:01 -0300

    Update main.jsx

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import { AuthProvider } from "@/contexts/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
